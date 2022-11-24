1286f1fd9c4ce9830068b761b4b79050a4a8eb86
Maidy Cabrera Penna <m.cabrerap@uniandes.edu.co>
2022-11-24 11:18:46 -0500

    added added logger and response route

---
+++ src/routes/response.route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const log = require('../services/logger.service')
+
+const router = new express.Router()
+
+router.post('/', async (req, res) => {
+  log.info('POST /pawl/v1/api/response')
+  const { body } = req
+  log.info(JSON.stringify(body))
+  res.status(201).send('OK')
+})
+
+module.exports = router
